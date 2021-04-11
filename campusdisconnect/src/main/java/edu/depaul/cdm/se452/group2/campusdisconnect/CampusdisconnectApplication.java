33f86714f89866486984a702eb7ef04ace2a6c82
salemij93 <salemi-j@hotmail.com>
2021-04-11 10:14:14 -0500

    adding build files and intial settup

---
+++ campusdisconnect/src/main/java/edu/depaul/cdm/se452/group2/campusdisconnect/CampusdisconnectApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.group2.campusdisconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusdisconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusdisconnectApplication.class, args);
+	}
+
+}
---
+++ campusdisconnect/src/test/java/edu/depaul/cdm/se452/group2/campusdisconnect/CampusdisconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.group2.campusdisconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusdisconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
