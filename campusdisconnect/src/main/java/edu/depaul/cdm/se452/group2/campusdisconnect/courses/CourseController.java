fc6ecae7c389cbeee5311a6c575b5043c0b0d5f2
Depaul <Depaul@Saras-MacBook-Pro.local>
2021-05-27 21:48:31 -0400

    sara changes

--- campusdisconnect/src/main/java/edu/depaul/cdm/se452/group2/campusdisconnect/courses/CourseController.java
+++ campusdisconnect/src/main/java/edu/depaul/cdm/se452/group2/campusdisconnect/courses/CourseController.java
@@ -1,12 +1,32 @@
 package edu.depaul.cdm.se452.group2.campusdisconnect.courses;
 
 
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.depaul.cdm.se452.group2.campusdisconnect.courses.Course;
+import edu.depaul.cdm.se452.group2.campusdisconnect.courses.CourseNoSQL;
+import edu.depaul.cdm.se452.group2.campusdisconnect.courses.CourseNoSQLRepository;
+import edu.depaul.cdm.se452.group2.campusdisconnect.courses.CourseRepository;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/course")
 public class CourseController {
 
@@ -18,10 +38,26 @@ public class CourseController {
 
 
     @CrossOrigin(origins = "http://localhost:8080")
-    @GetMapping("/info")
-    public String getCourseInfo(@RequestParam Long id) {
+    @GetMapping("/info/{id}")
+    public String getCourseInfo(@PathVariable Long id, Model model) {
         Course course = courseRepository.findBycourseid(id);
-        return "coursePage";
+
+
+        model.addAttribute("courseid", course.getCourseid());
+    	model.addAttribute("courseName", course.getCourseName());
+    	model.addAttribute("StartTime", course.getStartTime());
+    	model.addAttribute("EndTime", course.getEndTime());
+    	model.addAttribute("credits", course.getCredits());
+    	model.addAttribute("year", course.getYear());
+    	model.addAttribute("quarter", course.getQuarter());
+    	
+    
+    return "Course Info";
+
+
+
+
+
     }
 
 }
